ae6dbfe21fbf7724c52bc5f2d539a5af1c677b72
shubham kashyap <shubham.kashyap3026@gmail.com>
2026-09-24 14:23:40 +0530

    update ( array & problems )  -adding another way of solving palindromic number problem withut even  converting it into string

--- 3.language/JS/BasicPrograms/9.palindromicNumber.js
+++ 3.language/JS/BasicPrograms/9.palindromicNumber.js
@@ -20,3 +20,22 @@ function isPalindrome(x) {
 console.log(isPalindrome(121)) // true
 console.log(isPalindrome(-121)) // false
 console.log(isPalindrome(10)) // false
+
+
+/**
+ * actully writing a program that check is a number is palindromic or not without converting it to string
+ */
+function isPalindromeWithoutString(x) {
+    if (x < 0) return false;
+    let temp = 0
+    let num = x;
+    while (num > 0) {
+        temp = temp * 10 + num % 10;
+        num = Math.floor(num / 10);
+    }
+    return x < 0 ? -temp === x : temp === x;
+}
+
+console.log(isPalindromeWithoutString(121)) // true
+console.log(isPalindromeWithoutString(-121)) // false
+console.log(isPalindromeWithoutString(10)) // false
